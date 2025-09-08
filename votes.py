c9eecb894d985ded023a9a4b6f44a589cf837000
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2025-09-08 11:52:37 +0200

    pagination in votes

--- votes.py
+++ votes.py
@@ -1,58 +1,47 @@
 #!/usr/bin/env python3
 
-import bech32
 import requests
-import sys
+import urllib.parse
 
 
 def get_validators():
-    """
-    Fetches the list of all validators from the RPC endpoint.
-    Returns:
-        List of validators' operator addresses.
-    """
-    url = f"{BASE_URL}/cosmos/staking/v1beta1/validators"
-    response = requests.get(url)
-    if response.status_code == 200:
+    return paginate(f"{BASE_URL}/cosmos/staking/v1beta1/validators", "validators")
+
+
+def paginate(baseurl, items_key):
+    next_key = ""
+    items = []
+    while True:
+        url = f"{baseurl}?pagination.key={next_key}"
+        response = requests.get(url)
+        if response.status_code != 200:
+            print(f"Error fetching {url}: {response.status_code}")
         data = response.json()
-        validators = data.get("validators", [])
-        return validators
-        # return [validator["operator_address"] for validator in validators]
-    else:
-        print(f"Error fetching validators: {response.status_code}")
-        return []
+        items += data.get(items_key, [])
+        next_key = data.get("pagination").get("next_key")
+        # print(f"{url} received {len(items)} next_key={next_key}")
+        if next_key is None:
+            return items
+        next_key = urllib.parse.quote_plus(next_key)
 
 
 def get_votes():
-    url = f"{BASE_URL}/atomone/gov/v1/proposals/7/votes?limit=1000"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        votes = data.get("votes", [])
-        return votes
-        # return [validator["operator_address"] for validator in validators]
-    else:
-        print(f"Error fetching votes: {response.status_code}")
-        return []
+    return paginate(f"{BASE_URL}/atomone/gov/v1/proposals/15/votes", "votes")
 
 
 def get_delegator_delegations(addr):
-    url = f"{BASE_URL}/cosmos/staking/v1beta1/delegations/{addr}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        dels = data.get("delegation_responses", [])
-        return dels
-    else:
-        print(f"Error fetching validator delegations: {response.status_code}")
-        return []
+    return paginate(
+        f"{BASE_URL}/cosmos/staking/v1beta1/delegations/{addr}",
+        "delegation_responses",
+    )
 
 
 def main():
     global BASE_URL
-    BASE_URL = "https://atomone-testnet-1-api.allinbits.services"
+    BASE_URL = "https://atomone-api.allinbits.services"
     print("Fetching all votes...")
     votes = get_votes()
+    print(f"Fetched {len(votes)} votes.")
 
     if not votes:
         print("No votes found.")
